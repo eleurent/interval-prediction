Fix tau computation and plot several trajectories for frequency-based int

scripts/interval.py
@@ -60,25 +60,28 @@ class LP(object):
         dx = A @ x + B @ d
         return x + dx * dt
 
-    def trajectory(self, args):
-        # t = np.random.rand()
-        # x = np.array(t*self.x_i[0] + (1-t)*self.x_i[1])
-        x = np.array(self.x0)
-        xx = np.zeros((np.size(time), np.size(x)))
-
-        t = np.random.rand()
-        # d = np.array(t*self.d_i[0] + (1-t)*self.d_i[1])
-
+    def trajectory(self, args, random=True):
+        if random:
+            t = np.random.rand()
+            x = np.array(t*self.x_i[0] + (1-t)*self.x_i[1])
+        else:
+            x = np.array(self.x0)
 
         # Forward coordinates change
+        xx = np.zeros((np.size(time), np.size(x)))
         A = args
         self.update_coordinates_frame(A)
         x = self.change_coordinates(x)
         A = self.change_coordinates(A, matrix=True)
         B = self.change_coordinates(self.B, matrix=False) if self.B is not None else 0
 
+        if random:
+            r, phi = 2*np.random.rand()-1, np.random.rand()
+        else:
+            r, phi = 1, 0
+
         for i in range(np.size(time)):
-            d = np.array(self.d(time[i])) if self.d is not None else 0
+            d = r*np.array(self.d(time[i] + phi)) if self.d is not None else 0
             d = self.change_coordinates(d, offset=False)
             xx[i] = x
             x = self.step((A, B, d), x)
@@ -134,7 +137,7 @@ class LP(object):
     def mesh(self, n):
         vertices = [self.A0 + dAi for dAi in self.dAs]
         if len(vertices) == 1:
-            return vertices
+            return [vertices[0]]*n
         if len(vertices) == 2:
             return [(1-t) * vertices[0] + t*vertices[1] for t in np.linspace(0, 1, n)]
         for _ in range(2):
@@ -171,7 +174,7 @@ class LP(object):
 
     @property
     def tau(self):
-        return -4/np.min(np.linalg.eigvals(self.A0))
+        return 4/np.min(np.abs(np.linalg.eigvals(self.A0)))
 
     def asymptotic_bound(self, frequency, eps=0.05):
         return np.absolute(np.linalg.inv(1j*frequency*np.eye(self.A0.shape[0]) - self.A0) @ np.squeeze(self.B)) \

scripts/main.py
@@ -26,16 +26,16 @@ def main(trajectories=False,
     fig, axes = plt.subplots(sys.x0.size, 1, sharex=True, squeeze=False, figsize=(8, 4*sys.x0.size))
     # Trajectories
     if trajectories:
-        sys_mesh = sys.mesh(1)
+        sys_mesh = sys.mesh(50)
         t_color = next(palette)
         for i in range(sys.x0.size):
-            axes[i, 0].plot(time, sys.trajectory(sys_mesh[0])[:, i],
+            axes[i, 0].plot(time, sys.trajectory(sys_mesh[0], random=False)[:, i],
                             color=t_color,
                             alpha=1,
                             label=r"$x(t)$")
         for mesh_point in sys_mesh[1:]:
             for i in range(sys.x0.size):
-                axes[i, 0].plot(time, sys.trajectory(mesh_point)[:, i],
+                axes[i, 0].plot(time, sys.trajectory(mesh_point, random=True)[:, i],
                                 color=t_color,
                                 alpha=0.2)
 
@@ -53,18 +53,19 @@ def main(trajectories=False,
                             alpha=0.3)
 
             # Asymptotic enhancement
-            phase_2 = range(int(sys.tau/dt), 2*int(sys.tau/dt))
-            phase_3 = range(2*int(sys.tau/dt), np.size(time))
-            interval[phase_2, :, i] = [-sys.asymptotic_bound(0)[i], sys.asymptotic_bound(0)[i]]
-            interval[phase_3, :, i] = [-sys.asymptotic_bound(7)[i], sys.asymptotic_bound(7)[i]]
-            axes[i, 0].plot(time, interval[:, :, i],
-                            linestyle="dashed",
-                            linewidth=2,
-                            color=color,
-                            label=r"$\underline{x}(t), \overline{x}(t)$")
+            if True:
+                phase_2 = range(int(0.5*sys.tau/dt), int(1*sys.tau/dt))
+                phase_3 = range(int(1*sys.tau/dt), np.size(time))
+                interval[phase_2, :, i] = [-sys.asymptotic_bound(0)[i], sys.asymptotic_bound(0)[i]]
+                interval[phase_3, :, i] = [-sys.asymptotic_bound(7)[i], sys.asymptotic_bound(7)[i]]
+                axes[i, 0].plot(time, interval[:, :, i],
+                                linestyle="dashed",
+                                linewidth=2,
+                                color=color,
+                                label=r"$\underline{x}(t), \overline{x}(t)$")
+                plt.xticks([0, 0.5*sys.tau, sys.tau], ["0", r"$0.5\tau$", r"$\tau$"])
 
     # Display
-
     display = (0, 1)
     handles, labels = axes[0, 0].get_legend_handles_labels()
     plt.figlegend([handle for i, handle in enumerate(handles) if i in display],